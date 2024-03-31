8088768974618d9d54e324a3e8023cfb5d83fb2c
Artem Voevodin <voevodin320@gmail.com>
2024-03-31 22:44:31 +0200

    six cards added/ remove btn logic added

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
-// @todo: Темплейт карточки
-
 // @todo: DOM узлы
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
 
+// @todo: Темплейт карточки
 // @todo: Функция создания карточки
+function addCard(cardTitle, cardImage) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__title').textContent = cardTitle;
+  cardElement.querySelector('.card__image').src = cardImage;
+  cardElement.querySelector('.card__image').alt = `На изображении ` + cardTitle;
+
+  deleteButton.addEventListener('click', function() {
+    deleteCard(deleteButton);
+  })
+
+  placesList.append(cardElement);
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(referenceElement) {
+  const cardToRemove = referenceElement.closest('.card');
+  cardToRemove.remove()
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+  addCard(item.name, item.link);
+})
